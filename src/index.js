ba4c56be7ba2fc9b71d178dc6d5c9b92bd1c075b
Rio Trilaksono Putro <riotrilaksonop@gmail.com>
2021-03-28 02:15:54 +0700

    feat: testing calling graphql-pokeapi server

--- src/index.js
+++ src/index.js
@@ -4,6 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://graphql-pokeapi.vercel.app/api/graphql',
+  cache: new InMemoryCache()
+});
+
+client.query({
+  query: gql`
+  query GetPokemonList {
+    pokemons(limit:10, offset:0){
+      count
+      nextOffset
+      prevOffset
+      status
+      message
+      results{
+        id
+        name
+        image
+      }
+    }
+  }`
+})
+.then(result => console.log(result));
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
